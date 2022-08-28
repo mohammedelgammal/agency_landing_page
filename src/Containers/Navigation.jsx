55723585a21cc1cc1bc6558885c647db138dee30
mohammed_elgammal <you@example.com>
2022-08-28 23:09:35 +0200

    restructuring_Navigation

--- src/Containers/Navigation.jsx
+++ src/Containers/Navigation.jsx
@@ -2,11 +2,13 @@ import { Grid } from "@chakra-ui/react";
 import NavList from "./../Components/NavList";
 import NavBtns from "./../Components/NavBtns";
 
-const QuoteBtn = () => (
-  <Grid templateColumns="repeat(5, auto)" gap={7}>
-    <NavList />
-    <NavBtns />
-  </Grid>
-);
+const QuoteBtn = () => {
+  return (
+    <Grid templateColumns="repeat(5, auto)" gap={7}>
+      <NavList />
+      <NavBtns />
+    </Grid>
+  );
+};
 
 export default QuoteBtn;
