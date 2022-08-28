5d1adf765928e0dde61725e8cbaa290a61fc704c
mohammed_elgammal <you@example.com>
2022-08-29 01:21:53 +0200

    copying_showcaseContent

--- src/Components/ShowcaseText.jsx
+++ src/Components/ShowcaseText.jsx
@@ -1,8 +1,13 @@
 import { Center } from "@chakra-ui/react";
-import content from "./Utils/DataLists/showcaseContent";
 import Text from "./Text";
 
 const ShowcaseText = () => {
+  const content = {
+    small: "NO-CODE IS THE FUTURE!",
+    heading: "Why we build on Bubble",
+    paragraph:
+      "Airdev's mission is to enable non-coders to bring their ideas to life. Since most of our clients are, we picked a platform that would be easy for them to live on once we hand things over. Bubble is the clear choice because it allow you to build customizable web apps with pixel-perfect designs.",
+  };
   const { small, heading, paragraph } = content;
 
   return (
