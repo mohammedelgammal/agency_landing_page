c985ac023ad0db1990306490c4f4d56d86fe7c09
mohammed_elgammal <you@example.com>
2022-08-29 01:25:00 +0200

    moving_problemContent

--- src/Components/ProblemText.jsx
+++ src/Components/ProblemText.jsx
@@ -1,9 +1,16 @@
 import { Center } from "@chakra-ui/react";
-import content from "./Utils/DataLists/problemsContent";
 import Text from "../Components/Text";
 
 const ProblemText = () => {
+  const content = {
+    small: "THE PROBLEM",
+    heading:
+      "Software is eating the world... but software development is broken",
+    paragraph:
+      "Software powers today's economy but the way it is created is still stuck in the dark ages. Writing millions of lines of code to build products results in a process that is slow, expensive, unreliable, and inaccessible to most. Those familiar with software development know to expect:",
+  };
   const { small, heading, paragraph } = content;
+  
   return (
     <Center flexDirection={"column"} w={"1280px"}>
       <Text
